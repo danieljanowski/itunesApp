34a36b30e830c8b0cf69cf8d0b883ffb7e9ba801
danieljanowski <daniel.janowski@gmail.com>
2023-03-01 15:16:15 +0000

    added second screen

---
+++ Apple iTunes/DetailedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailedViewController.swift
+//  Apple iTunes
+//
+//  Created by Daniel Janowski on 01/03/2023.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
